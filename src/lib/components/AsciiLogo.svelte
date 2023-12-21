4815dced0327be524f4873479134856b3d4031cc
coppinger <charles.coppinger@gmail.com>
2023-12-22 09:04:49 +1300

    git init

---
+++ src/lib/components/AsciiLogo.svelte
@@ -0,0 +1,21 @@
+<pre class="text-[4px]">
+                                                                                                                                                
+                                                                                      dddddddd                                                  
+BBBBBBBBBBBBBBBBB                                                                     d::::::dKKKKKKKKK    KKKKKKK  iiii          tttt          
+B::::::::::::::::B                                                                    d::::::dK:::::::K    K:::::K i::::i      ttt:::t          
+B::::::BBBBBB:::::B                                                                   d::::::dK:::::::K    K:::::K  iiii       t:::::t          
+BB:::::B     B:::::B                                                                  d:::::d K:::::::K   K::::::K             t:::::t          
+  B::::B     B:::::B  aaaaaaaaaaaaa      ssssssssss       eeeeeeeeeeee        ddddddddd:::::d KK::::::K  K:::::KKKiiiiiiittttttt:::::ttttttt    
+  B::::B     B:::::B  a::::::::::::a   ss::::::::::s    ee::::::::::::ee    dd::::::::::::::d   K:::::K K:::::K   i:::::it:::::::::::::::::t    
+  B::::BBBBBB:::::B   aaaaaaaaa:::::ass:::::::::::::s  e::::::eeeee:::::ee d::::::::::::::::d   K::::::K:::::K     i::::it:::::::::::::::::t    
+  B:::::::::::::BB             a::::as::::::ssss:::::se::::::e     e:::::ed:::::::ddddd:::::d   K:::::::::::K      i::::itttttt:::::::tttttt    
+  B::::BBBBBB:::::B     aaaaaaa:::::a s:::::s  ssssss e:::::::eeeee::::::ed::::::d    d:::::d   K:::::::::::K      i::::i      t:::::t          
+  B::::B     B:::::B  aa::::::::::::a   s::::::s      e:::::::::::::::::e d:::::d     d:::::d   K::::::K:::::K     i::::i      t:::::t          
+  B::::B     B:::::B a::::aaaa::::::a      s::::::s   e::::::eeeeeeeeeee  d:::::d     d:::::d   K:::::K K:::::K    i::::i      t:::::t          
+  B::::B     B:::::Ba::::a    a:::::assssss   s:::::s e:::::::e           d:::::d     d:::::d KK::::::K  K:::::KKK i::::i      t:::::t    tttttt
+BB:::::BBBBBB::::::Ba::::a    a:::::as:::::ssss::::::se::::::::e          d::::::ddddd::::::ddK:::::::K   K::::::Ki::::::i     t::::::tttt:::::t
+B:::::::::::::::::B a:::::aaaa::::::as::::::::::::::s  e::::::::eeeeeeee   d:::::::::::::::::dK:::::::K    K:::::Ki::::::i     tt::::::::::::::t
+B::::::::::::::::B   a::::::::::aa:::as:::::::::::ss    ee:::::::::::::e    d:::::::::ddd::::dK:::::::K    K:::::Ki::::::i       tt:::::::::::tt
+BBBBBBBBBBBBBBBBB     aaaaaaaaaa  aaaa sssssssssss        eeeeeeeeeeeeee     ddddddddd   dddddKKKKKKKKK    KKKKKKKiiiiiiii         ttttttttttt  
+                              
+</pre>
