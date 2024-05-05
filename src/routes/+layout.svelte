3c2aa01ec8cf0ed6ef1c2ec8a4fdff2a884891b3
Charlie Coppinger <charles.coppinger@gmail.com>
2024-05-05 13:58:50 +1200

    add meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,16 @@
     import "../app.pcss";
 </script>
 
+<svelte:head>
+    <title>BasedKit</title>
+    <meta
+        name="description"
+        content="BasedKit is a free and open source boilerplate for building with Supabase (inc. Supabase CLI) and SvelteKit."
+    />
+    <link rel="icon" href="/favicon.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+</svelte:head>
+
 <div
     class="min-w-screen dark flex min-h-screen flex-col items-center justify-center gap-4 bg-black text-[#00ff00] p-2"
 >
